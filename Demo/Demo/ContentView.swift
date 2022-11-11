43839e4b098b8bc8a7f2b175630c76c7c042e5ea
Farren Springer <fjs507@gmail.com>
2022-11-11 12:36:13 -0500

    Demo-5 Moved Keychain struct to its own file.

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -56,34 +56,3 @@ func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: (
         return try withAnimation(animation, body)
     }
 }
-
-struct Keychain {
-    static func save(key: String, data: Data) -> OSStatus {
-        let query = [
-            kSecClass as String       : kSecClassGenericPassword as String,
-            kSecAttrAccount as String : key,
-            kSecValueData as String   : data ] as [String : Any]
-        
-        SecItemDelete(query as CFDictionary)
-        
-        return SecItemAdd(query as CFDictionary, nil)
-    }
-    
-    static func load(key: String) -> Data? {
-        let query = [
-            kSecClass as String       : kSecClassGenericPassword,
-            kSecAttrAccount as String : key,
-            kSecReturnData as String  : kCFBooleanTrue!,
-            kSecMatchLimit as String  : kSecMatchLimitOne ] as [String : Any]
-        
-        var dataTypeRef: AnyObject? = nil
-        
-        let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
-        
-        if status == noErr {
-            return dataTypeRef as! Data?
-        } else {
-            return nil
-        }
-    }
-}
