4e7b793013f7bd35927c3ad7a0e205879b5c90a7
Farren Springer <fjs507@gmail.com>
2022-11-11 09:04:59 -0500

    Demo-1 Added NavigationView and two placeholder NavigationLinks.

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -9,11 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationView {
+            List {
+                NavigationLink("Go to ViewA", destination: ViewA())
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationTitle("Go Back")
+                    .font(.title3)
+                    .frame(height: 30)
+                    .padding()
+                NavigationLink("Go to ViewA", destination: ViewB())
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationTitle("Go Back")
+                    .font(.title3)
+                    .frame(height: 30)
+                    .padding()
+            }
+        }
+    }
+}
+
+struct ViewA: View {
+    var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("This is view a")
+        }
+        .padding()
+    }
+}
+
+struct ViewB: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("This is view b")
         }
         .padding()
     }
@@ -24,3 +57,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
